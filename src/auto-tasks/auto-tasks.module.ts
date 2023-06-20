947092b78ffd6548e02e0b18b9a2fdd21cf7d2c9
keshoxvol <51890346+keshoxvol@users.noreply.github.com>
2023-06-20 20:01:42 +0300

    d

--- src/auto-tasks/auto-tasks.module.ts
+++ src/auto-tasks/auto-tasks.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AutoTasksService } from './auto-tasks.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Building } from 'src/Models/Building';
 
 @Module({
-  providers: [AutoTasksService]
+  providers: [AutoTasksService],
+  imports: [TypeOrmModule.forFeature([Building])]
 })
-export class AutoTasksModule {}
+export class AutoTasksModule { }
