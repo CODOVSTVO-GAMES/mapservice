b0bdead57cb8b1ce54c4aa946c2c12e554027d9a
keshoxvol <51890346+keshoxvol@users.noreply.github.com>
2023-06-23 14:27:24 +0300

    а

--- src/DTO/DataDTO.ts
+++ src/DTO/DataDTO.ts
@@ -4,18 +4,16 @@ export class DataDTO {
     x: number
     y: number
     level: number
-    battlesNumber: number
     battleOwner: string
     taskId: number
     taskStatus: number
 
-    constructor(accountId: string, zone: string, x: number, y: number, level: number, battlesNumber: number, battleOwner: string, taskId: number, taskStatus = 0) {
+    constructor(accountId: string, zone: string, x: number, y: number, level: number battleOwner: string, taskId: number, taskStatus = 0) {
         this.accountId = accountId
         this.zone = zone
         this.x = x
         this.y = y
         this.level = level
-        this.battlesNumber = battlesNumber
         this.battleOwner = battleOwner
         this.taskId = taskId
         this.taskStatus = taskStatus
