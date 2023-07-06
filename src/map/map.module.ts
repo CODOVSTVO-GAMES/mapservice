990c3ddaa81b3778e005a1083414d8b89ba0a743
keshoxvol <51890346+keshoxvol@users.noreply.github.com>
2023-07-06 15:40:40 +0300

    d

--- src/map/map.module.ts
+++ src/map/map.module.ts
@@ -1,9 +1,12 @@
 import { Module, Global } from '@nestjs/common';
 import { MapService } from './map.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Building } from 'src/Models/Building';
 
 @Global()
 @Module({
     exports: [MapService],
-    providers: [MapService]
+    providers: [MapService],
+    imports: [TypeOrmModule.forFeature([Building])]
 })
 export class MapModule { }
